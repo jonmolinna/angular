4c94c7789395d9f5ca26bf80c588371f5951f266
jonmolinna <jmolina2624@gmail.com>
2024-04-27 08:55:39 -0500

    Formularios Reactivos

--- 94-routing-angular/src/app/contact/contact.component.ts
+++ 94-routing-angular/src/app/contact/contact.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  // Formulario de Tipo Plantilla
+  public user: any = {
+    name: '',
+    email: '',
+  };
 
+  handleEnviar() {
+    console.log('YOOO', this.user);
+  }
+
+  // Formulario de tipo Reactivo
+  formInitial: FormGroup;
+
+  constructor(private form: FormBuilder) {
+    this.formInitial = this.form.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  handleSubmit() {
+    console.log('YOOOO', this.formInitial);
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    return (
+      this.formInitial.get(controlName)?.hasError(errorType) &&
+      this.formInitial.get(controlName)?.touched
+    );
+  }
 }
