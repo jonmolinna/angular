7884d59f7f0592cb4af1d1ee1e00ec448d1bd7ed
jonmolinna <jmolina2624@gmail.com>
2024-04-30 08:17:24 -0500

    Formularios

--- 94-routing-angular/src/app/contact/contact.component.ts
+++ 94-routing-angular/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   // Formulario de Tipo Plantilla
   public user: any = {
     name: '',
@@ -19,10 +19,19 @@ export class ContactComponent {
 
   // Formulario de tipo Reactivo
   formInitial: FormGroup;
+  // userActiv: any = {
+  //   name: 'Kendra',
+  //   surname: 'Contreras',
+  //   dni: '74382349',
+  // };
+  typeDni: string = 'DNI';
 
   constructor(private form: FormBuilder) {
     this.formInitial = this.form.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
+      surname: ['', [Validators.required, Validators.minLength(3)]],
+      typeDni: [''],
+      dni: [''],
       email: ['', [Validators.required, Validators.email]],
     });
   }
@@ -36,5 +45,33 @@ export class ContactComponent {
       this.formInitial.get(controlName)?.hasError(errorType) &&
       this.formInitial.get(controlName)?.touched
     );
+  }
+
+  // ngOnInit(): void {
+  //   // this.formInitial.get('name')?.setValue(this.nameUser);
+  //   this.formInitial
+  //     .get('dni')
+  //     ?.setValidators([Validators.required, Validators.minLength(3)]);
+
+  //   this.formInitial.patchValue({
+  //     name: this.userActiv.name,
+  //     surname: this.userActiv.surname,
+  //     dni: this.userActiv.dni,
+  //   });
+  //   this.formInitial.get('name')?.disable();
+  //   this.formInitial.get('surname')?.disable();
+  //   this.formInitial.get('dni')?.disable();
+  // }
+
+  ngOnInit(): void {
+    // subscribe -> el formulario escucha cada cambio
+    // Igual que react en el formulario
+    // this.formInitial.valueChanges.subscribe((valor) => {
+    //   console.log('toooo', valor);
+    // });
+
+    this.formInitial.get('typeDni')?.valueChanges.subscribe((value) => {
+      this.typeDni = value;
+    });
   }
 }
