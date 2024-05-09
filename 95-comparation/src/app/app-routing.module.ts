05f205454eda46e935762d0ca9f9c1cdfe4424c8
jonmolinna <jmolina2624@gmail.com>
2024-05-09 09:01:07 -0500

    Comparation

---
+++ 95-comparation/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'ahora',
+    loadChildren: () =>
+      import('./ahora/ahora.module').then((m) => m.AhoraModule),
+  },
+  {
+    path: 'antes',
+    loadChildren: () =>
+      import('./antes/antes.module').then((m) => m.AntesModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
