213ee9d367f1ed93decdaa128d7d5f47fbc52b0b
jonmolinna <jmolina2624@gmail.com>
2024-06-01 09:00:00 -0500

    Variables Observables Antes

--- 95-comparation/src/app/antes/variables/observables/observables.component.ts
+++ 95-comparation/src/app/antes/variables/observables/observables.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import productData from 'src/app/data/product.data';
+import cartInterface from 'src/app/interface/cart.interface';
+import productInterface from 'src/app/interface/product.iterface';
 
 @Component({
   selector: 'app-observables',
@@ -23,6 +26,15 @@ export class ObservablesComponent implements OnInit {
 
   nombreObservable: Observable<any> = this.name.asObservable();
   private nombreSubscription?: Subscription;
+
+  // ------------------------------------------------------------------------
+  private products: BehaviorSubject<productInterface[]> =
+    new BehaviorSubject<any>([]);
+
+  private carts: BehaviorSubject<cartInterface[]> = new BehaviorSubject<any>(
+    []
+  );
+  // ------------------------------------------------------------------------
 
   get numberOne(): any {
     return this.number1.getValue();
@@ -60,6 +72,31 @@ export class ObservablesComponent implements OnInit {
     this.nombre = data;
   }
 
+  // ---------------------------------------------------------------------
+  get getProducts(): productInterface[] {
+    return this.products.getValue();
+  }
+
+  get getCart(): cartInterface[] {
+    return this.carts.getValue();
+  }
+
+  set setCart(value: any) {
+    this.carts.next(value);
+  }
+
+  get getQuantityProductsToCart(): number {
+    return this.getCart.reduce((number, obj) => number + obj.quantity, 0);
+  }
+
+  get getPriceTotalCart(): number {
+    return this.getCart.reduce(
+      (number, obj) => number + obj.quantity * obj.price,
+      0
+    );
+  }
+  // ---------------------------------------------------------------------
+
   ngOnInit(): void {
     this.numeroSubscription1 = this.numeroObservable1.subscribe((newNumber) => {
       this.sumNumber = this.numberOne + this.numberTwo;
@@ -72,5 +109,46 @@ export class ObservablesComponent implements OnInit {
     this.nombreSubscription = this.nombreObservable.subscribe((newName) => {
       this.nombreModificado = this.nombre;
     });
+
+    // -------------------------------------------------
+    this.products.next(productData);
+  }
+
+  handleAddProductToCart(product: productInterface) {
+    const cart = this.getCart.find((item) => item.id === product.id);
+
+    if (cart) {
+      this.setCart = this.getCart.map((item) =>
+        item.id === cart.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    } else {
+      this.setCart = [...this.getCart, { ...product, quantity: 1 }];
+    }
+  }
+
+  addStockProduct(product: productInterface) {
+    const cart = this.getCart.find((item) => item.id === product.id);
+
+    if (cart) {
+      this.setCart = this.getCart.map((item) =>
+        item.id === cart.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    }
+  }
+
+  restStockProduct(product: productInterface) {
+    const cart = this.getCart.find((item) => item.id === product.id);
+
+    if (cart) {
+      this.setCart = this.getCart.map((item) =>
+        item.id === cart.id
+          ? { ...item, quantity: item.quantity < 2 ? 1 : item.quantity - 1 }
+          : item
+      );
+    }
+  }
+
+  deleteProductToCart(id: number) {
+    this.setCart = this.getCart.filter((item) => item.id !== id);
   }
 }
