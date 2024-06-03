5f47cb29f457485d7d970888d274aad9c1e1af9c
jonmolinna <jmolina2624@gmail.com>
2024-06-03 08:46:41 -0500

    Variables Simple

--- 95-comparation/src/app/antes/variables/simple/simple.component.ts
+++ 95-comparation/src/app/antes/variables/simple/simple.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import productData from 'src/app/data/product.data';
+import cartInterface from 'src/app/interface/cart.interface';
+import productInterface from 'src/app/interface/product.iterface';
 
 @Component({
   selector: 'app-simple',
   templateUrl: './simple.component.html',
 })
-export class SimpleComponent {
+export class SimpleComponent implements OnInit {
   number1: number = 5;
   number2: number = 10;
   sumNumber = this.number1 + this.number2;
@@ -16,5 +19,62 @@ export class SimpleComponent {
 
   cambiarNumberTwo() {
     this.number2 = this.number2 + 10;
+  }
+
+  // ---------------------------------------------------------------
+  products: productInterface[] = [];
+  carts: cartInterface[] = [];
+
+  ngOnInit(): void {
+    this.products = productData;
+  }
+
+  handleAddProductToCart(product: productInterface) {
+    const cart = this.carts.find((item) => item.id === product.id);
+
+    if (cart) {
+      this.carts = this.carts.map((item) =>
+        item.id === cart.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    } else {
+      this.carts = [...this.carts, { ...product, quantity: 1 }];
+    }
+  }
+
+  addStockProduct(product: productInterface) {
+    const cart = this.carts.find((item) => item.id === product.id);
+
+    if (cart) {
+      this.carts = this.carts.map((item) =>
+        item.id === cart.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    }
+  }
+
+  restStockProduct(product: productInterface) {
+    const cart = this.carts.find((item) => item.id === product.id);
+
+    if (cart) {
+      this.carts = this.carts.map((item) =>
+        item.id === cart.id
+          ? { ...item, quantity: item.quantity < 2 ? 1 : item.quantity - 1 }
+          : item
+      );
+    }
+  }
+
+  deleteToCartProduct(id: number) {
+    this.carts = this.carts.filter((item) => item.id !== id);
+  }
+
+  getProductTotal(): number {
+    return this.carts.reduce((number, obj) => number + obj.quantity, 0);
+  }
+
+  getPriceTotalCart(): number {
+    return this.carts.reduce(
+      (number, obj) => number + obj.price * obj.quantity,
+      0
+    );
   }
 }
